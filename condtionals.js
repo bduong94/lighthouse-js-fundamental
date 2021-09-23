bacd40b604318d6a987607090142501c48a7a9e7
Brian Duong <brianminhduong@gmail.com>
2021-09-23 22:24:00 +0000

    Practice with logical operators

--- condtionals.js
+++ condtionals.js
@@ -19,14 +19,33 @@
 //     console.log('Short sleeves are fine.');
 // }
 
-const temperature = -15;
+// const temperature = -15;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18){
+//     console.log('You are eligible to vote.');
+// }
+
+// const temperature = -20;
+
+// if (temperature < -40 || temperature > 40){
+//     console.log('Maybe going outside isn\'t such a great idea...');
+// }
+
+const raining = false;
+
+if (!raining){
+    console.log('Leave your umbrella at home!');
 }
-
-console.log("Now you're ready to go outside!");
