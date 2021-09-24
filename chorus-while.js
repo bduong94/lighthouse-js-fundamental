a99f5a2c47e223a9eb086f75d2c54e87afdd5c8d
Brian Duong <brianminhduong@gmail.com>
2021-09-24 00:36:11 +0000

    First while loop with chorus

---
+++ chorus-while.js
@@ -0,0 +1,20 @@
+const chorus = 'Let\'s dance';
+
+let repeat = 0;
+
+while (repeat < 10){
+
+
+    if (repeat === 5){
+
+        console.log('*change key*');
+
+    }
+
+    console.log(chorus); //Prints out chorus
+
+    repeat ++; //Increments repeat by 1
+
+}
+
+console.log('Until the sun comes up!');
