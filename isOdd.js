8c7c06535bddacd5294a4fe6a879679b65c338df
Brian Duong <brianminhduong@gmail.com>
2021-09-25 18:37:10 +0000

    Odd function - Returns true if number is odd or false if number is even

---
+++ isOdd.js
@@ -0,0 +1,16 @@
+const isOdd = function(num) { //Assigning nameless function to isOdd variable
+
+  if (num % 2 === 0){ //Checks to see if even
+
+    return false
+
+  }
+
+  return true //The number is odd
+
+}
+
+
+// //Cases
+// console.log("3 is odd: " + isOdd(3));
+// console.log("8 is odd: " + isOdd(8));
